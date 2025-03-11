fe66cf8ae0e955fc9dae31f31b1cfa3a55256a03
Andrey Zasimov <zasimovandrey2000@gmail.com>
2025-03-11 18:01:46 +0300

    Add plugins functional

--- AFK_PC.py
+++ AFK_PC.py
@@ -2,6 +2,7 @@ import configparser
 import ctypes
 import os
 import sys
+import importlib.util
 
 import win32con
 import win32gui
@@ -12,6 +13,7 @@ OFFLINE_CHECK_INTERVAL = 1
 PAUSE_CHECK_INTERVAL = 1200
 EXCEPTION_APPS = []
 AUTO_START = 0
+PLUGINS = []
 
 LAST_WINDOWS = []
 PAUSED = False
@@ -23,7 +25,8 @@ class LastInputInfo(ctypes.Structure):
 
 
 def load_settings():
-    global AFK_TIME, ONLINE_CHECK_INTERVAL, OFFLINE_CHECK_INTERVAL, PAUSE_CHECK_INTERVAL, EXCEPTION_APPS, AUTO_START
+    global AFK_TIME, ONLINE_CHECK_INTERVAL, OFFLINE_CHECK_INTERVAL, PAUSE_CHECK_INTERVAL, EXCEPTION_APPS, \
+        AUTO_START, PLUGINS
     config = configparser.ConfigParser()
     config.read('config.ini')
     AFK_TIME = int(config['DEFAULT']['AFK_TIME'])
@@ -32,6 +35,7 @@ def load_settings():
     PAUSE_CHECK_INTERVAL = int(config['DEFAULT']['PAUSE_CHECK_INTERVAL'])
     EXCEPTION_APPS = config['DEFAULT']['EXCEPTION_APPS'][1:-1].replace("'", '').split(', ')
     AUTO_START = int(config['DEFAULT']['AUTO_START'])
+    PLUGINS = config['DEFAULT']['PLUGINS'][1:-1].replace("'", '').split(', ')
 
 
 def save_settings():
@@ -41,7 +45,8 @@ def save_settings():
                          'OFFLINE_CHECK_INTERVAL': str(OFFLINE_CHECK_INTERVAL),
                          'PAUSE_CHECK_INTERVAL': str(PAUSE_CHECK_INTERVAL),
                          'EXCEPTION_APPS': str(EXCEPTION_APPS),
-                         'AUTO_START': str(AUTO_START)}
+                         'AUTO_START': str(AUTO_START),
+                         'PLUGINS': str(PLUGINS)}
     with open('config.ini', 'w') as configfile:
         config.write(configfile)
 
@@ -99,6 +104,94 @@ def get_absolute_path_to_exe_file():
     return os.path.abspath(sys.argv[0])
 
 
+def run_pause_plugins_functions():
+    try:
+        for plugin_path in PLUGINS:
+            spec = importlib.util.spec_from_file_location("plugin", plugin_path)
+            plugin = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(plugin)
+            plugin.pause()
+    except Exception:
+        pass
+
+
+def run_afk_enter_plugin_functions():
+    try:
+        for plugin_path in PLUGINS:
+            spec = importlib.util.spec_from_file_location("plugin", plugin_path)
+            plugin = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(plugin)
+            plugin.afk_enter()
+    except Exception:
+        pass
+
+
+def run_afk_exit_plugin_functions():
+    try:
+        for plugin_path in PLUGINS:
+            spec = importlib.util.spec_from_file_location("plugin", plugin_path)
+            plugin = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(plugin)
+            plugin.afk_exit()
+    except Exception:
+        pass
+
+
+def run_main_plugin_functions():
+    try:
+        for plugin_path in PLUGINS:
+            spec = importlib.util.spec_from_file_location("plugin", plugin_path)
+            plugin = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(plugin)
+            plugin.main()
+    except Exception:
+        pass
+
+
+def run_start_plugin_functions():
+    try:
+        for plugin_path in PLUGINS:
+            spec = importlib.util.spec_from_file_location("plugin", plugin_path)
+            plugin = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(plugin)
+            plugin.start()
+    except Exception:
+        pass
+
+
+def run_exit_plugin_functions():
+    try:
+        for plugin_path in PLUGINS:
+            spec = importlib.util.spec_from_file_location("plugin", plugin_path)
+            plugin = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(plugin)
+            plugin.exit()
+    except Exception:
+        pass
+
+
+def run_afk_plugin_functions():
+    try:
+        for plugin_path in PLUGINS:
+            spec = importlib.util.spec_from_file_location("plugin", plugin_path)
+            plugin = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(plugin)
+            plugin.afk()
+    except Exception:
+        pass
+
+
+def run_active_plugin_functions():
+    try:
+        for plugin_path in PLUGINS:
+            spec = importlib.util.spec_from_file_location("plugin", plugin_path)
+            plugin = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(plugin)
+            plugin.active()
+    except Exception:
+        pass
+
+
 def add_to_startup():
     from win32com.client import Dispatch
     startup_folder = os.path.join(os.environ["APPDATA"], "Microsoft", "Windows", "Start Menu", "Programs", "Startup")
@@ -123,7 +216,8 @@ def remove_from_startup():
 def settings():
     import customtkinter as ctk
 
-    global AFK_TIME, ONLINE_CHECK_INTERVAL, OFFLINE_CHECK_INTERVAL, PAUSE_CHECK_INTERVAL, EXCEPTION_APPS, AUTO_START
+    global AFK_TIME, ONLINE_CHECK_INTERVAL, OFFLINE_CHECK_INTERVAL, PAUSE_CHECK_INTERVAL, EXCEPTION_APPS, \
+        AUTO_START, PLUGINS
 
     on_check_intervals = {"Очень редко": 30, "Редко": 10, "Часто": 5, "Довольно часто": 3, "Очень часто": 1}
     off_check_intervals = {"Очень медленно": 5, "Медленно": 3, "Быстро": 2,
@@ -132,7 +226,8 @@ def settings():
                              "Довольно часто": 100, "Очень часто": 50}
 
     def save_close():
-        global AFK_TIME, ONLINE_CHECK_INTERVAL, OFFLINE_CHECK_INTERVAL, PAUSE_CHECK_INTERVAL, EXCEPTION_APPS, AUTO_START
+        global AFK_TIME, ONLINE_CHECK_INTERVAL, OFFLINE_CHECK_INTERVAL, PAUSE_CHECK_INTERVAL, EXCEPTION_APPS, \
+            AUTO_START, PLUGINS
 
         try:
             AFK_TIME = int(afk_time_entry.get())
@@ -141,6 +236,7 @@ def settings():
             PAUSE_CHECK_INTERVAL = pause_check_intervals[pause_check_interval_cmb.get()]
             EXCEPTION_APPS = exception_apps_widgets
             AUTO_START = int(auto_start_var.get())
+            PLUGINS = plugins
             save_settings()
 
             if AUTO_START:
@@ -184,6 +280,38 @@ def settings():
 
                 exception_apps_widgets.append(name)
 
+    def add_plugin(file_path=None):
+        def delete_plugin(_frame, _file_path):
+            _frame.destroy()
+            plugins.remove(_file_path)
+
+        if file_path:
+            frame = ctk.CTkFrame(plugins_frame, fg_color="#333333", corner_radius=8)
+            frame.pack(fill='x', pady=10, padx=10)
+
+            label = ctk.CTkLabel(frame, text=f"{'.'.join(file_path.split('/')[-2:])[:-3]}")
+            label.pack(side="left", padx=5, pady=5)
+
+            delete_button = ctk.CTkButton(frame, text="X", width=28,
+                                          command=lambda: delete_plugin(frame, file_path))
+            delete_button.pack(side="right", padx=5, pady=5)
+
+        else:
+            from tkinter import filedialog
+            file_path = filedialog.askopenfilename(title="Выберите файл", filetypes=[("Python файлы", "*.py")])
+            if file_path not in plugins:
+                frame = ctk.CTkFrame(plugins_frame, fg_color="#333333", corner_radius=8)
+                frame.pack(fill='x', pady=10, padx=10)
+
+                label = ctk.CTkLabel(frame, text=f"{'.'.join(file_path.split('/')[-2:])[:-3]}")
+                label.pack(side="left", padx=5, pady=5)
+
+                delete_button = ctk.CTkButton(frame, text="X", width=28,
+                                              command=lambda: delete_plugin(frame, file_path))
+                delete_button.pack(side="right", padx=5, pady=5)
+
+                plugins.append(file_path)
+
     open_applications_names = [str(win32gui.GetWindowText(i)) for i in get_open_windows()]
     if "Окно переполнения области задач." in open_applications_names:
         open_applications_names.remove('Окно переполнения области задач.')
@@ -191,10 +319,11 @@ def settings():
         open_applications_names.remove('Program Manager')
 
     exception_apps_widgets = EXCEPTION_APPS
+    plugins = PLUGINS
 
     window = ctk.CTk()
     window.title("Настройки")
-    window.geometry("380x610")
+    window.geometry("380x940")
 
     afk_time_label = ctk.CTkLabel(window, text="Время бездействия (сек):")
     afk_time_label.grid(row=0, column=0, padx=10, pady=10, sticky='w')
@@ -242,16 +371,27 @@ def settings():
     exception_apps_frame = ctk.CTkScrollableFrame(window)
     exception_apps_frame.grid(row=7, column=0, columnspan=2, padx=10, pady=10, sticky='ew')
 
+    plugins_label = ctk.CTkLabel(window, text="Приложения исключения:")
+    plugins_label.grid(row=8, column=0, columnspan=2, padx=10, pady=10, sticky='w')
+
+    plugins_button = ctk.CTkButton(window, text="Добавить", command=add_plugin)
+    plugins_button.grid(row=9, column=1, padx=10, pady=10, sticky='w')
+
+    plugins_frame = ctk.CTkScrollableFrame(window)
+    plugins_frame.grid(row=10, column=0, columnspan=2, padx=10, pady=10, sticky='ew')
+
     save_button = ctk.CTkButton(window, text="Сохранить", command=save_close)
-    save_button.grid(row=8, column=0, padx=10, pady=10, sticky='w')
+    save_button.grid(row=11, column=0, padx=10, pady=10, sticky='w')
 
     quit_button = ctk.CTkButton(window, text="Отмена", command=window.destroy)
-    quit_button.grid(row=8, column=1, padx=10, pady=10, sticky='w')
+    quit_button.grid(row=11, column=1, padx=10, pady=10, sticky='w')
 
     if '' in exception_apps_widgets:
         exception_apps_widgets.remove('')
     for exc in exception_apps_widgets:
         add_exception_application(exc)
+    for plug in plugins:
+        add_plugin(plug)
 
     window.mainloop()
 
@@ -262,6 +402,8 @@ def pause():
 
 
 def exit_program(tray):
+    run_exit_plugin_functions()
+    save_settings()
     tray.stop()
     sys.exit()
 
@@ -269,7 +411,7 @@ def exit_program(tray):
 def tray_setup():
     from pystray import MenuItem, Icon
     from PIL import Image
-    image = Image.new("RGB", (64, 64), (255, 0, 0))
+    image = Image.new("RGB", (64, 64), (0, 0, 255))
     menu = (
         MenuItem('Настройки', settings),
         MenuItem('Пауза', pause),
@@ -284,25 +426,35 @@ def mainloop():
     global PAUSED, AFK
 
     while True:
+        if "" in PLUGINS:
+            PLUGINS.remove("")
+
         if PAUSED:
+            run_pause_plugins_functions()
             sleep(PAUSE_CHECK_INTERVAL)
             continue
 
         if get_afk_time() >= AFK_TIME and not is_watching_video() and not is_exception_application():
+            run_afk_plugin_functions()
             if not AFK:
+                run_afk_enter_plugin_functions()
                 minimize_windows()
                 AFK = True
             sleep(OFFLINE_CHECK_INTERVAL)
         else:
+            run_active_plugin_functions()
             if AFK:
+                run_afk_exit_plugin_functions()
                 maximize_windows()
                 AFK = False
             sleep(ONLINE_CHECK_INTERVAL)
+        run_main_plugin_functions()
 
 
 def main():
     from threading import Thread
     load_settings()
+    run_start_plugin_functions()
     Thread(target=mainloop, daemon=True).start()
     tray_setup()
 
